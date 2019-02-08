cd044c16f90bbe441185024ef9ce5370069e70ed
ciervo <ciervo@example.com>
2019-02-08 16:08:12 +0100

    Primer commit

---
+++ java.php
@@ -0,0 +1,66 @@
+<?php include 'cabecera.inc'; ?>
+
+<h1>Java</h1>
+<hr/>
+
+<p class="text-justify">
+Java es un lenguaje de programación de propósito general, concurrente, orientado a objetos, que fue diseñado específicamente para tener tan pocas dependencias de 
+implementación como fuera posible. Su intención es permitir que los desarrolladores de aplicaciones escriban el programa una vez y lo ejecuten en cualquier 
+dispositivo (conocido en inglés como WORA, o "write once, run anywhere"), lo que quiere decir que el código que es ejecutado en una plataforma no tiene que ser 
+recompilado para correr en otra. Java es, a partir de 2012, uno de los lenguajes de programación más populares en uso, particularmente para aplicaciones de 
+cliente-servidor de web, con unos diez millones de usuarios reportados.<br/><br/>
+El lenguaje de programación Java fue originalmente desarrollado por James Gosling, de Sun Microsystems (constituida en 1982 y posteriormente adquirida el 27 de enero
+de 2010 por la compañía Oracle),4​ y publicado en 1995 como un componente fundamental de la plataforma Java de Sun Microsystems. Su sintaxis deriva en gran medida de 
+C y C++, pero tiene menos utilidades de bajo nivel que cualquiera de ellos. Las aplicaciones de Java son compiladas a bytecode (clase Java), que puede ejecutarse en 
+cualquier máquina virtual Java (JVM) sin importar la arquitectura de la computadora subyacente.<br/><br/>
+La compañía Sun desarrolló la implementación de referencia original para los compiladores de Java, máquinas virtuales y librerías de clases en 1991, y las publicó por 
+primera vez en 1995. A partir de mayo de 2007, en cumplimiento de las especificaciones del Proceso de la Comunidad Java, Sun volvió a licenciar la mayoría de sus 
+tecnologías de Java bajo la Licencia Pública General de GNU. Otros también han desarrollado implementaciones alternas a estas tecnologías de Sun, tales como el 
+Compilador de Java de GNU y el GNU Classpath. 
+</p>
+
+<img src="https://cdn-images-1.medium.com/max/1600/1*7Kqu2b7kSmpj45g4X1_DKA.jpeg" class="img-fluid" alt="Responsive image" width="100%">
+
+<hr/>
+<h1>Historia de Java</h1>
+<hr/>
+
+
+<div class="embed-responsive embed-responsive-16by9">
+  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/RUEnLW3Cu6g" allowfullscreen></iframe>
+</div>
+
+
+<hr/>
+<h1>Características de Java</h1>
+<hr/>
+
+<p class="text-justify">
+El lenguaje Java se creó con cinco objetivos principales:<br/><br/>
+1. Debería usar el paradigma de la programación orientada a objetos.<br/>
+2. Debería permitir la ejecución de un mismo programa en múltiples sistemas operativos.<br/>
+3. Debería incluir por defecto soporte para trabajo en red.<br/>
+4. Debería diseñarse para ejecutar código en sistemas remotos de forma segura.<br/>
+5. Debería ser fácil de usar y tomar lo mejor de otros lenguajes orientados a objetos, como C++.<br/><br/>
+Para conseguir la ejecución de código remoto y el soporte de red, los programadores de Java a veces recurren a extensiones como CORBA (Common Object Request Broker 
+Architecture), Internet Communications Engine u OSGi, respectivamente.<br/><br/>
+
+<b>Orientado a objetos</b><br/><br/>
+La primera característica, orientado a objetos (“OO”), se refiere a un método de programación y al diseño del lenguaje. Aunque hay muchas interpretaciones para OO, 
+una primera idea es diseñar el software de forma que los distintos tipos de datos que usen estén unidos a sus operaciones. Así, los datos y el código (funciones o métodos) 
+se combinan en entidades llamadas objetos. Un objeto puede verse como un paquete que contiene el “comportamiento” (el código) y el “estado” (datos). 
+El principio es separar aquello que cambia de las cosas que permanecen inalterables. Frecuentemente, cambiar una estructura de datos implica un cambio en el código 
+que opera sobre los mismos, o viceversa. Esta separación en objetos coherentes e independientes ofrece una base más estable para el diseño de un sistema software. 
+El objetivo es hacer que grandes proyectos sean fáciles de gestionar y manejar, mejorando como consecuencia su calidad y reduciendo el número de proyectos fallidos.
+<br/><br/>
+Otra de las grandes promesas de la programación orientada a objetos es la creación de entidades más genéricas (objetos) que permitan la reutilización del software 
+entre proyectos, una de las premisas fundamentales de la Ingeniería del Software. Un objeto genérico “cliente”, por ejemplo, debería en teoría tener el mismo conjunto 
+de comportamiento en diferentes proyectos, sobre todo cuando estos coinciden en cierta medida, algo que suele suceder en las grandes organizaciones. En este sentido, 
+los objetos podrían verse como piezas reutilizables que pueden emplearse en múltiples proyectos distintos, posibilitando así a la industria del software construir proyectos 
+de envergadura empleando componentes ya existentes y de comprobada calidad, conduciendo esto finalmente a una reducción drástica del tiempo de desarrollo. 
+Podemos usar como ejemplo de objeto el aluminio. Una vez definidos datos (densidad, maleabilidad, etc.), y su “comportamiento” (soldar dos piezas, etc.), 
+el objeto “aluminio” puede ser reutilizado en el campo de la construcción, del automóvil, de la aviación, etc.
+
+</p>
+
+<?php include 'pie.inc'; ?> 
